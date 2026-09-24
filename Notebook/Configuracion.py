1d1fda7063b656835f4120a5b88e398d8c60b1d9
Rundrayer <fernandofuentesg15@gmail.com>
2026-09-23 20:21:48 -0600

    Eliminación de datasets pesados

--- Notebook/Configuracion.py
+++ Notebook/Configuracion.py
@@ -1,6 +1,26 @@
 #CONFIGURACION DE RUTAS DE PROYECTO
 
+#
 from pathlib import Path
+import os
+
+
+#MODIFICAR RUTAS CON LOS ARCHIVOS winutils.exe y hadoop.dll -------------------------------------------------------------------MODIFICAR
+RUTA_HADOOP = Path(r"C:\Users\galle\Desktop\IA Datos\Jupyter\Proyecto_GDIABD_Cybersecurity_Data_Analytics\Recursos\hadoop")
+
+#--
+
+# Asignación automática de variables de entorno para PySpark/Hadoop
+if RUTA_HADOOP.exists():
+  os.environ["HADOOP_HOME"] = str(RUTA_HADOOP.resolve())
+  os.environ["PATH"] = (
+      os.environ["HADOOP_HOME"] + r"\bin;" + os.environ.get("PATH", "")
+  )
+else:
+  print(
+      f"ADVERTENCIA: La ruta de Hadoop '{RUTA_HADOOP}' no existe. Revisa Configuracion.py"
+  )
+
 
 #APUNTA A LA CARPETA "Datos" UBICADA JUNTO CON ESTE PROYECTO
 DIRECTORIO_BASE = Path("../Datos") 
@@ -12,7 +32,7 @@ RECURSOS = Path("../Recursos")
 CARGADOS = DIRECTORIO_BASE / "Cargados"
 PROCESADOS = DIRECTORIO_BASE / "Procesados"
 CSV_GENERADOS = DIRECTORIO_BASE / "CSV_Generados"
-IMPLEMENTACION_GOBERNANZA = DIRECTORIO_BASE / "Implementacion_Gobernaza"
+IMPLEMENTACION_GOBERNANZA = DIRECTORIO_BASE / "Implementacion_Gobernanza"
 ANALITICOS_PARQUET = DIRECTORIO_BASE / "Analiticos" / "Parquet"
 ANALITICOS_RESULTADOS = DIRECTORIO_BASE / "Analiticos" / "Resultados"
 
